248d55b1ff769ccde5516cc3f0460acc0abfcef7
DangFiztssi <ngdang31@gmail.com>
2016-11-08 16:04:16 +0700

    Main

---
+++ app/src/main/java/com/minhnpa/coderschool/a9boarding/fragment/main/HomeFragment.java
@@ -0,0 +1,105 @@
+package com.minhnpa.coderschool.a9boarding.fragment.main;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.minhnpa.coderschool.a9boarding.R;
+import com.minhnpa.coderschool.a9boarding.adapter.PostViewHolder;
+import com.minhnpa.coderschool.a9boarding.model.Post;
+import com.minhnpa.coderschool.a9boarding.presenter.HomePresenter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.facebook.login.widget.ProfilePictureView.TAG;
+
+public class HomeFragment extends Fragment {
+
+    @BindView(R.id.rvMain)
+    RecyclerView rvMain;
+
+    HomePresenter presenter;
+
+    FirebaseRecyclerAdapter<Post, PostViewHolder> adapter;
+
+//    private OnFragmentInteractionListener mListener;
+
+    private DatabaseReference mDatabaseReference;
+
+    public HomeFragment() {
+    }
+
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, view);
+
+        presenter = new HomePresenter(getContext());
+
+        if (rvMain != null) {
+            RecyclerView.LayoutManager manager = new LinearLayoutManager(getContext());
+            rvMain.setHasFixedSize(true);
+            rvMain.setLayoutManager(manager);
+
+//            adapter = new FirebaseRecyclerAdapter<Post, PostViewHolder>(Post.class, R.layout.item_post, PostViewHolder.class, FirebaseDatabase.getInstance().getReference().child("posts")) {
+//                @Override
+//                protected void populateViewHolder(PostViewHolder viewHolder, Post model, int position) {
+//                    Log.e(TAG, "populateViewHolder: " + model );
+//                }
+//            };
+
+            rvMain.setAdapter(presenter.getAdapter());
+        } else {
+            Log.e(TAG, "rv null ");
+        }
+
+        return view;
+    }
+
+    public void onButtonPressed(Uri uri) {
+//        if (mListener != null) {
+//            mListener.onFragmentInteraction(uri);
+//        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+//        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
--- app/src/main/java/com/minhnpa/coderschool/a9boarding/model/Post.java
+++ app/src/main/java/com/minhnpa/coderschool/a9boarding/model/Post.java
@@ -1,67 +1,21 @@
 package com.minhnpa.coderschool.a9boarding.model;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
-import com.google.gson.annotations.SerializedName;
-
 import java.util.List;
 
-public class Post implements Parcelable {
-    private long postId;
+public class Post {
+    private long post_id;
     private User user;
-    private String createdAt;
+    private String created_at;
     private String address;
     private String price;
     private List<Image> images;
-    private long favouritesCount;
+    private long favourites_count;
     private String description;
-    private Information infoContact;
+    private Information infomation_contact;
 
-    private Post(Parcel in) {
-        postId = in.readLong();
-        user = in.readParcelable(User.class.getClassLoader());
-        createdAt = in.readString();
-        address = in.readString();
-        price = in.readString();
-        images = in.createTypedArrayList(Image.CREATOR);
-        favouritesCount = in.readLong();
-        description = in.readString();
-        infoContact = in.readParcelable(Information.class.getClassLoader());
-    }
-
-    public static final Creator<Post> CREATOR = new Creator<Post>() {
-        @Override
-        public Post createFromParcel(Parcel in) {
-            return new Post(in);
-        }
-
-        @Override
-        public Post[] newArray(int size) {
-            return new Post[size];
-        }
-    };
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeLong(postId);
-        dest.writeParcelable(user, flags);
-        dest.writeString(createdAt);
-        dest.writeString(address);
-        dest.writeString(price);
-        dest.writeTypedList(images);
-        dest.writeLong(favouritesCount);
-        dest.writeString(description);
-        dest.writeParcelable(infoContact, flags);
-    }
 
     public long getPostId() {
-        return postId;
+        return post_id;
     }
 
     public User getUser() {
@@ -69,7 +23,7 @@ public class Post implements Parcelable {
     }
 
     public String getCreatedAt() {
-        return createdAt;
+        return created_at;
     }
 
     public String getAddress() {
@@ -85,7 +39,7 @@ public class Post implements Parcelable {
     }
 
     public long getFavouritesCount() {
-        return favouritesCount;
+        return favourites_count;
     }
 
     public String getDescription() {
@@ -93,6 +47,6 @@ public class Post implements Parcelable {
     }
 
     public Information getInfoContact() {
-        return infoContact;
+        return infomation_contact;
     }
 }
